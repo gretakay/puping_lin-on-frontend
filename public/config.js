adf2ec99763b530d4bf4379b163e996a44c6dd11
嚴俞涵 Greta.yan <greta.yan@rinnai.com.tw>
2026-08-19 14:00:29 +0800

    Fix: Use correct GAS deployment URL

--- public/config.js
+++ public/config.js
@@ -1,7 +1,8 @@
 // GAS API 配置
 // 用戶只需修改這一個地方，所有頁面都會使用
 
-window.GAS_API_URL = 'https://script.google.com/macros/d/1mBXkiJGZJ9RI_9UNo2SLduoSaSGIe6aGWk4mHRn1zOqWNrnBMHYbNW36/usercontent';
+window.GAS_API_URL = 'https://script.google.com/macros/s/AKfycbx3H3tiQ44DYtyfge22k3PdmTTxteC6bmTB38wU5pzXm7mdmcdZdv2NBesVHxBECM78/exec';
+
 
 
 
